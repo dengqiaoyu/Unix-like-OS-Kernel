2913bf38a6fcd509b6b0a6528126a536424e4631
dengqiaoyu <deng.qiaoyu@gmail.com>
2017-02-19 23:44:07 -0500

    made some modifications

--- p2/user/libthread/thread.c
+++ p2/user/libthread/thread.c
@@ -4,6 +4,8 @@
 
 #include <stdlib.h>
 #include <types.h>
+#include <thread.h>
+#include <syscall.h>
 #include "mutex.h"
 #include "cond.h"
 #include "thr_internals.h"
@@ -23,103 +25,106 @@ mutex_t counter_mutex;
 mutex_t *group_mutexes;
 allocator_t **stack_allocators;
 
-int thr_init(int size)
-{
-  stack_size = size;
-  tid_counter = 0;
-  if (mutex_init(&tid_mutex) != 0) return -1;
+// int thr_init(int size);
+// int thr_create(void *(*func)(void *), void *args);
+// int thr_join(int tid, void **statusp);
+// void thr_exit( void *status );
+// int thr_getid(void);
+// int thr_yield(int user_tid);
 
-  stack_allocators = (allocator_t **)calloc(NUM_THREAD_LISTS, sizeof(void *));
-  if (stack_allocators == NULL) return -1;
-  int i;
-  for (i = 0; i < NUM_THREAD_LISTS; i++) {
-    if ((stack_allocators[i] = malloc(sizeof(allocator_t))) == NULL) return -1;
-    if (allocator_init(stack_allocators[i], stack_size, LIST_CHUNK_NUM))
-      return -1;
-  }
+int thr_init(int size) {
+    stack_size = size;
+    tid_counter = 0;
+    if (mutex_init(&tid_mutex) != 0) return -1;
 
-  group_mutexes = (mutex_t *)calloc(num_lists, sizeof(mutex_t));
-  if (group_mutexes == NULL) return -1;
-  for (i = 0; i < num_lists; i++) {
-    if (mutex_init(&(group_mutexes[i])) != 0) return -1;
-  }
+    stack_allocators = (allocator_t **)calloc(NUM_THREAD_LISTS, sizeof(void *));
+    if (stack_allocators == NULL) return -1;
+    int i;
+    for (i = 0; i < NUM_THREAD_LISTS; i++) {
+        if ((stack_allocators[i] = malloc(sizeof(allocator_t))) == NULL) return -1;
+        if (allocator_init(stack_allocators[i], stack_size, LIST_CHUNK_NUM))
+            return -1;
+    }
 
-  if (thread_table_init() < 0) return -1;
-  return 0;
+    group_mutexes = (mutex_t *)calloc(num_lists, sizeof(mutex_t));
+    if (group_mutexes == NULL) return -1;
+    for (i = 0; i < num_lists; i++) {
+        if (mutex_init(&(group_mutexes[i])) != 0) return -1;
+    }
+
+    if (thread_table_init() < 0) return -1;
+    return 0;
 }
 
-int thr_create(void *(*func)(void *), void *args)
-{
-  mutex_lock(&counter_mutex);
-  int stack_count = stack_counter++;
-  mutex_unlock(&counter_mutex);
+int thr_create(void *(*func)(void *), void *args) {
+    mutex_lock(&counter_mutex);
+    int stack_count = stack_counter++;
+    mutex_unlock(&counter_mutex);
 
-  int stack_group = GET_STACK_GROUP(stack_count);
-  if (stack_allocators == NULL) return -1; // impossible
-  if (stack_allocators[stack_group] == NULL) return -1; // impossible
+    int stack_group = GET_STACK_GROUP(stack_count);
+    if (stack_allocators == NULL) return -1; // impossible
+    if (stack_allocators[stack_group] == NULL) return -1; // impossible
 
-  mutex_lock(&(group_mutexes[stack_group]));
-  void *new_stack = allocator_alloc(stack_allocators[stack_group]);
-  if (new_stack == NULL) return -1;
-  void *stack_bottom = (void *)((char *)new_stack + stack_size);
-  int tid = start_thread(stack_bottom, func, args);
+    mutex_lock(&(group_mutexes[stack_group]));
+    void *new_stack = allocator_alloc(stack_allocators[stack_group]);
+    if (new_stack == NULL) return -1;
+    void *stack_bottom = (void *)((char *)new_stack + stack_size);
+    int tid = start_thread(stack_bottom, func, args);
 
-  thr_info *tinfo = thread_table_insert(tid);
-  if (tinfo == NULL) return -1;
-  tinfo->tid = tid;
-  tinfo->stack_group = stack_group;
-  tinfo->stack = new_stack;
-  tinfo->exited = 0;
-  mutex_unlock(&(group_mutexes[stack_group]));
-  return tid;
+    thr_info *tinfo = thread_table_insert(tid);
+    if (tinfo == NULL) return -1;
+    tinfo->tid = tid;
+    tinfo->stack_group = stack_group;
+    tinfo->stack = new_stack;
+    tinfo->exited = 0;
+    mutex_unlock(&(group_mutexes[stack_group]));
+    return tid;
 }
 
 int thr_join(int tid, void **statusp) {
-    thread_info *thr_to_join = thread_table_find(tid);
+    thr_info *thr_to_join = thread_table_find(tid);
     if (thr_to_join == NULL) {
         return ERROR_THREADS_COULD_NOT_FIND_BY_ID;
     }
-    if (thr_to_join->join_thr_id > 0) {
-        mutex_unlock(thr_to_join->mutex);
+    if (thr_to_join->join_tid > 0) {
+        mutex_unlock(&(thr_to_join->mutex));
         return ERROR_THREADS_ALREADY_JOINED;
     }
 
-    thr_to_join->join_thr_id == thr_getid();
+    thr_to_join->join_tid = thr_getid();
     // THREAD_EXITED, THREAD_CLEARED
-    if (thr_to_join->status != THREAD_EXIT) {
-        cond_wait(thr_to_join->cond, thr_to_join->mutex);
+    if (thr_to_join->state != EXIT) {
+        cond_wait(&(thr_to_join->cond), &(thr_to_join->mutex));
     }
 
-    if (thr_to_join->return_val != NULL) {
-        *statusp = thr_to_join->return_val;
+    if (thr_to_join->status != NULL) {
+        *statusp = thr_to_join->status;
     }
-    mutex_unlock(thr_to_join->mutex);
-    allocator_free(thr_to_join->stack_base);
-    allocator_free(thr_to_join);
-    delete_thr(tid);
+    mutex_unlock(&(thr_to_join->mutex));
+    allocator_free(thr_to_join->stack);
+    thread_table_delete(thr_to_join);
     return SUCCESS;
 }
 
 void thr_exit( void *status ) {
-    int tid = thr_getid();
-    thread_info *thr_to_exit = thread_table_find(tid);
+    int kernel_tid = gettid();
+    thr_info *thr_to_exit = thread_table_find(kernel_tid);
     // Is it impossible to get thread_info?
-    thr_to_exit->status = THREADS_EXIT;
-    thr_to_exit->return_val = status;
+    thr_to_exit->state = EXIT;
+    thr_to_exit->status = status;
 
-    if (thr_to_exit->join_thr_id > 0) {
-        mutex_unlock(thr_to_exit->mutex);
-        cond_signal(thr_to_exit->cond);
+    if (thr_to_exit->join_tid > 0) {
+        mutex_unlock(&(thr_to_exit->mutex));
+        cond_signal(&(thr_to_exit->cond));
     }
-    mutex_unlock(thr_to_exit->mutex);
+    mutex_unlock(&(thr_to_exit->mutex));
+    vanish();
 }
 
 int thr_getid(void) {
-    int kernel_tid = gettid();
-    return get_user_tid(kernel_tid);
+    return gettid();
 }
 
-int thr_yield(int user_tid) {
-    int kernel_tid = get_kernel_tid(user_tid);
-    return yield(kernel_tid);
+int thr_yield(int tid) {
+    return yield(tid);
 }
--- p2/user/libthread/thread_table.h
+++ p2/user/libthread/thread_table.h
@@ -4,17 +4,25 @@
 
 #ifndef _TID_TABLE_H_
 #define _TID_TABLE_H_
+#include "mutex.h"
+#include "cond.h"
 
 typedef struct thread_info thr_info;
 struct thread_info {
-  int tid;
-  int stack_group;
-  void *stack;
-  cond_t cv;
-  int status;
-  void *retval;
-  struct node *next;
+    int counter;
+    int kernel_tid;
+    void *stack;
+    mutex_t mutex;
+    cond_t cond;
+    int join_tid;
+    int state;
+    void *status;
 };
+
+// status CODE
+#define RUNNABLE 0
+#define SUSPEND 1
+#define EXIT 2
 
 int thread_table_init(int num_thread_lists);
 thr_info *thread_table_find(int tid);
