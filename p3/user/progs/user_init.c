736aadf9fea5316447dbcc8bb1ffc456558d7a0c
Newton Xie <newtoncxie@gmail.com>
2017-04-15 12:03:49 -0400

    Forgot to add user_init.c.

---
+++ p3/user/progs/user_init.c
@@ -0,0 +1,29 @@
+#include <syscall.h>
+#include <stdio.h>
+#include <simics.h>
+
+int main()
+{
+    int pid, exitstatus;
+    char test[] = "actual_wait";
+    /*
+    char test[] = "wild_test1";
+    char test[] = "remove_pages_test2";
+    char test[] = "cho";
+    char test[] = "fork_exit_bomb";
+    char test[] = "fork_wait_bomb";
+    char test[] = "actual_wait";
+    */
+    char * args[] = {test, 0};
+
+    while(1) {
+        pid = fork();
+        if (!pid)
+            exec(test, args);
+
+        while (pid != wait(&exitstatus));
+
+        printf("Shell exited with status %d; starting it back up...", exitstatus);
+        MAGIC_BREAK;
+    }
+}
